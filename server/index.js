671843c1e2ab8ffd14577550d23dfeb13cb31fd7
Misbah Aslam <159120706+misbah-aslam16@users.noreply.github.com>
2025-05-06 21:17:32 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
 		origin: ['http://localhost:5174',
 			 'http://localhost:5173',
-			'https://e-commerce-website-eosin-two.vercel.app',
+			'https://hakcheck-ktmc.vercel.app',
 			
 			],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
